92152606994867e881b6e0517e35fce3ab333fa5
MrDB <bnuik77212@gmail.com>
2022-07-16 21:31:31 +0900

    커스텀 슬라이드 바 추가

--- src/Program.cs
+++ src/Program.cs
@@ -4,12 +4,12 @@ using System.Drawing;
 
 public class Program {
     static TextReplayer trply = new TextReplayer();
-    static bool isRecord = false;
+    static bool bRecord = false;
 
     internal static void Main(string[] args){
         Form form = new Form();
         form.Text = "Replay Tester";
-        form.Size = new Size(700,500);
+        form.Size = new Size(700,520);
 
         // Adding TextBox
         TextBox textBox     = new TextBox();
@@ -18,7 +18,7 @@ public class Program {
         textBox.Multiline   = true;
         textBox.Anchor      = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom;
         textBox.TextChanged+= delegate(object sender,EventArgs args){
-            if( isRecord ){
+            if( bRecord ){
                 try{
                     // Save time data
                     trply.AddRecordPoint(textBox.Text);
@@ -29,49 +29,50 @@ public class Program {
         // Adding Controls GroupBox
         GroupBox controlBox = new GroupBox();
         controlBox.Location = new Point(10,380);
-        controlBox.Size     = new Size(665,70);
+        controlBox.Size     = new Size(665,90);
         controlBox.Text     = "Controls";
         controlBox.Anchor   = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Bottom;
 
         // Adding Slider
-        TrackBar timeSlider      = new TrackBar();
-        timeSlider.Location      = new Point(5,28);
+        SlideBar timeSlider      = new SlideBar();
+        timeSlider.Location      = new Point(5,25);
+        timeSlider.Size          = new Size(480,16);
         timeSlider.AutoSize      = false;
-        timeSlider.Width         = 480;
-        timeSlider.Height        = 30;
-        timeSlider.Maximum       = 100;
-        timeSlider.TickFrequency = 10;
         timeSlider.Anchor        = AnchorStyles.Left | AnchorStyles.Right;
-        timeSlider.LargeChange  = 1;
-        timeSlider.SmallChange   = 1;
-        // Hide the tick indicator
-        timeSlider.TickStyle     = TickStyle.None;
-        timeSlider.Scroll       += delegate(object sender,EventArgs args){
-            // Restore time data
-            try{
-                textBox.Text = trply.GetDataFromTime(timeSlider.Value);
-            }catch{}
+        timeSlider.Slide        += delegate(object sender, EventArgs args){
+            if( !bRecord ){
+                try{
+                    // Restore time data
+                    textBox.Text = trply.GetDataFromTime( timeSlider.Value );
+                }catch{}
+            }
         };
 
         // Adding Recording Button
         Button startRecordBtn    = new Button();
         startRecordBtn.Location  = new Point(500,25);
-        startRecordBtn.Size      = new Size(150,30);
+        startRecordBtn.Size      = new Size(150,32);
         startRecordBtn.Text      = "Start Recording";
         startRecordBtn.Anchor    = AnchorStyles.Right;
         startRecordBtn.Click    += delegate(object sender,EventArgs  args){
-            isRecord = !isRecord;
-            if(isRecord){
+            bRecord = !bRecord;
+            if(bRecord){
                 trply.StartRecord(textBox.Text);
                 ((Button)sender).Text = "Stop Recording";
             }else{
-                timeSlider.Maximum = (int)trply.StopRecord();
+                timeSlider.Maximum = trply.StopRecord();
                 ((Button)sender).Text = "Start Recording";
             }
         };
+
+        // Adding Pause/Resume Button
+        Button prBtn   = new Button();
+        prBtn.Location = new Point(5,50);
+
         // Adding Children 
         controlBox.Controls.Add(timeSlider);
         controlBox.Controls.Add(startRecordBtn);
+        controlBox.Controls.Add(prBtn);
         
         // Adding Children
         form.Controls.Add( textBox );
---
+++ src/SlideBar.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+public class SlideBar : Panel{
+    private bool   m_bDragState = false;
+    private float  m_fDragDelta = 0.0f;
+    private float  m_fBackDelta = 0.0f;
+    public  Panel  Thumb        = null;
+    public  long   Maximum      = 100;
+    public  long   Value        = 0;
+    /*
+    * 커스텀 이벤트 핸들러
+    */
+    public EventHandler Slide;
+    /*
+    * 내부 슬라이드 막대 요소를 생성하는 클래스 생성자
+    */
+    public SlideBar(){
+        this.BackgroundImage = Image.FromFile("./resources/slider_back.png");
+        this.BackgroundImageLayout = ImageLayout.Stretch;
+
+        this.Thumb                  = new Panel();
+        Thumb.Size                  = new Size(16,16);
+        Thumb.Location              = new Point(0,0);
+        Thumb.BackColor             = Color.Transparent;
+        Thumb.BackgroundImage       = Image.FromFile("./resources/slider_thumb.png");
+        Thumb.BackgroundImageLayout = ImageLayout.Stretch;
+        Thumb.Enabled               = false; // 막대 트래커(Thumb)를 비활성화 안하면, 트래커 위에서 부모 컨트롤의 마우스 이벤트가 적용 X
+
+        // 막대 트래커(Thumb)의 드래그 기능 구현 (마우스 이벤트 사용)
+        this.MouseMove   += delegate(object sender, MouseEventArgs args){
+            // 막대 트래커(Thumb) 위치가 슬라이드 바 영역을 벗어나면, 예외처리
+            if( args.X < 0 || args.X > (this.Width - Thumb.Width) ){
+                return;
+            }
+            if( m_bDragState ){
+                Thumb.Left   = args.X;
+                // 현재 슬라이드 바의 넓이와 막대 트래커(Thumb) 위치를 기반으로 현재 슬라이드의 값을 계산
+                m_fDragDelta = (float)Math.Round( ((float)args.X / (float)this.Width) * Maximum ); 
+                // 동일한 값이 다시 들어오면 입력 방지
+                if( m_fBackDelta != m_fDragDelta ){
+                    m_fBackDelta = m_fDragDelta;
+                    this.Value   = (long)m_fDragDelta;
+                    // 커스텀 스크롤 이벤트 발생
+                    Slide?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        };
+
+        this.MouseDown  += delegate(object sender, MouseEventArgs args){
+            Thumb.Left   = args.X;
+            m_bDragState = true;
+        };
+
+        this.MouseUp    += delegate(object sender, MouseEventArgs args){
+            m_bDragState = false;
+        };
+        
+        // 부모 요소의 크기(Size) 값이 변경 될 때마다, 막대 트래커(Thumb)의 크기를 재조정
+        this.SizeChanged += delegate(object sender, EventArgs args){
+            Thumb.Size  = new Size((sender as Panel).Height,(sender as Panel).Height);
+        };
+
+        this.Controls.Add(Thumb);
+    }
+}
